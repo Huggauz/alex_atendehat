ad5f8122950a1fd9c2b19a8e455306b2af48c197
Huggauz <hugo@gauzz.com.mx>
2025-10-01 14:26:52 -0600

    agregar alex

---
+++ frontend/src/translate/i18n.js
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { messages } from "./languages";
+
+const savedLanguage = localStorage.getItem('i18nextLng') || 'es';
+
+i18n.init({
+	debug: true,
+	defaultNS: ["translations"],
+	fallbackLng: "en",
+	ns: ["translations"],
+	resources: messages,
+	lng: savedLanguage,
+});
+
+export const changeLanguage = (language) => {
+	i18n.changeLanguage(language);
+	localStorage.setItem('i18nextLng', language);
+};
+
+export { i18n };
